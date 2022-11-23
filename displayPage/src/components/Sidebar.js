b8923b584289a215c2a1c49fb6371d8a12fd2f31
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2022-11-23 14:45:10 +0100

    sidebar andd car cards

--- displayPage/src/components/Sidebar.js
+++ displayPage/src/components/Sidebar.js
@@ -1,12 +1,20 @@
 import React from "react"
 import styled from "styled-components"
-
+import Card from "react-bootstrap/Card"
+import data from "../data/carData.json"
 
 export default function Sidebar(){
+    let years = data.map((d)=>d.Year.split("-")[0])
+    let otheryears = new Set(years)
+
     return(
         <Wrapper>
             <div className="main">
                 <h1>Tags</h1>
+                <h2> Years ( {otheryears.size} ) </h2>
+                <ul>
+                    {otheryears.size > 0 ? Array.from(otheryears).map((year) => (<li>{year}</li>)) : null}
+                </ul>
             </div>
         </Wrapper>
     )
@@ -22,6 +30,19 @@ const Wrapper = styled.section`
         height: 100vh;
         margin-left: 3px;
         text-align: center;
+    }
+
+    h1 {
+        text-decoration: underline;
+        margin-bottom: 30px;
+    }
+    h2 {
+        text-decoration: underline;
+    }
+
+    ul{
+        list-style-type: none;
+        font-size: 30px;
     }
 
 `
