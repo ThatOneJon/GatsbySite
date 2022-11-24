5d181deb1de37e80be6d9fbed469b59ce135eab4
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2022-11-24 18:42:05 +0100

    SEO

---
+++ displayPage/src/components/SEO.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {Helmet} from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+
+const SEO = () => {
+    return(
+        <Helmet title="Cars">
+            
+        </Helmet>
+    )
+}
+export default SEO
--- displayPage/src/pages/index.js
+++ displayPage/src/pages/index.js
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/Layout"
 import Container from "react-bootstrap/Container"
 import CarData from "../components/CarData"
-
+import SEO from "../components/SEO"
 
 export default function Home() {
   return (
       <Layout>
+        <SEO />
         <Container fluid="sm" style={{backgroundColor:"#"}}>
           < CarData />
         </Container>
